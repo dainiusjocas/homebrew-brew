0ee62533e27669c9049df950d1676e73749ca4de
Dainius Jocas <dainius.jocas@gmail.com>
2021-09-24 12:40:23 +0300

    update ket to 2021.09.24

--- ket.rb
+++ ket.rb
@@ -2,14 +2,14 @@ class Ket < Formula
 
   desc "KET - Kafka Elasticsearch Tool"
   homepage "https://github.com/vinted/kafka-elasticsearch-tool"
-  version "2021.05.23"
+  version "2021.09.24"
 
   if OS.linux?
-    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.05.23/ket-v2021.05.23-linux-amd64.zip"
-    sha256 "03e1011dd18b961f865c47469b382ea0aaf8b7ddbc5ca99d8481a205bc3d4b12"
+    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.09.24/ket-v2021.09.24-linux-amd64.zip"
+    sha256 "c55409b9860a8d8f8c50e08aa2c17203c62bb7c42d95cebf3c363c5feed61ab9"
   else
-    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.05.23/ket-v2021.05.23-macos-amd64.zip"
-    sha256 "cadce7f06e9e20703a260deef060d7756098a999c5cac481f7190fea815e3b1c"
+    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.09.24/ket-v2021.09.24-macos-amd64.zip"
+    sha256 "2c82b06039f6f0aa5899ff0c9809fad7cac186087980cc0904da766b470d8b7e"
   end
 
   bottle :unneeded
