1af976d86ec755c6c0ef73060f5fb18382b6684f
Dainius Jocas <dainius.jocas@gmail.com>
2021-11-02 00:16:54 +0200

    chore: update lmgrep

--- lmgrep.rb
+++ lmgrep.rb
@@ -2,14 +2,14 @@ class Lmgrep < Formula
 
   desc "Grep-like utility based on Lucene Monitor"
   homepage "https://github.com/dainiusjocas/lucene-grep"
-  version "2021.05.23"
+  version "2021.11.01"
 
   if OS.linux?
-    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.05.23/lmgrep-v2021.05.23-linux-amd64.zip"
-    sha256 "b4c87f7e154f182e879b650ce8f3bedfaf27be236dd081df0197a3da1de5f8b3"
+    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.11.01/lmgrep-v2021.11.01-linux-amd64-static.zip"
+    sha256 "1a17f6740e4217fc1177578ee974ac813109e0d1a1e61015728419817911564c"
   else
-    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.05.23/lmgrep-v2021.05.23-macos-amd64.zip"
-    sha256 "b45e2c00e12329d859384d5ee1ecdf0f8f0bccdf80afb2660448ea3244a284d5"
+    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.11.01/lmgrep-v2021.11.01-macos-amd64.zip"
+    sha256 "2626a1e3ce261a0b1e9716db7e9f0378d220c817e52b44042f3cc8e308d49d27"
   end
 
   bottle :unneeded
