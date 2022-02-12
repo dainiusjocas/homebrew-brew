ed68ccb9982d2fd907e4eef9a3bd27570d5ecabd
Dainius Jocas <dainius.jocas@vinted.com>
2022-02-12 10:54:56 +0200

    chore: update lmgrep to v2022.02.10

--- lmgrep.rb
+++ lmgrep.rb
@@ -2,14 +2,14 @@ class Lmgrep < Formula
 
   desc "Grep-like utility based on Lucene Monitor"
   homepage "https://github.com/dainiusjocas/lucene-grep"
-  version "2021.12.09"
+  version "2022.02.10"
 
   if OS.linux?
-    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.12.09/lmgrep-v2021.12.09-linux-amd64-static.zip"
-    sha256 "ad4b47b6693dce72bab7df0d28f6f76bf6e799c5eed3f0e296ebef9c60f8ad29"
+    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.02.10/lmgrep-v2022.02.10-linux-amd64-static.zip"
+    sha256 "a8ebb50391c0fde638ebf423bca1f72c0cc54dbc824bbe6d6d90d04bc35c1b16"
   else
-    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.12.09/lmgrep-v2021.12.09-macos-amd64.zip"
-    sha256 "c0bc941ab98815e1370002848bb07a7e1450d94dfaddc23304e732e618749e94"
+    url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.02.10/lmgrep-v2022.02.10-macos-amd64.zip"
+    sha256 "fc60eb8e0cc960becf3930300d9dedb6c2b746a3daea7e36473de47cb3828a9f"
   end
 
   def install
