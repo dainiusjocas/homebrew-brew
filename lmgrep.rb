d56150c0c76b0660dc1654d6c59e494f589a26f9
Dainius Jocas <dainius.jocas@vinted.com>
2022-02-14 17:06:14 +0200

    fix: lmgrep sha not matching

--- lmgrep.rb
+++ lmgrep.rb
@@ -6,10 +6,10 @@ class Lmgrep < Formula
 
   if OS.linux?
     url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.02.14/lmgrep-v2022.02.14-linux-amd64-static.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "dd7c5bfacb04db94da8706a8e9423b514d5dec85098979ccfbb0cce78e5ed4cb"
   else
     url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.02.14/lmgrep-v2022.02.14-macos-amd64.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "cf9dbc3558586b7414593f07766cc984445439c17c9f3c4386ba9ca87cf2b2ed"
   end
 
   def install
