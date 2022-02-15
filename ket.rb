6cc0870c2c99afa85a2156ebf239881bb7cf4d10
Dainius Jocas <dainius.jocas@gmail.com>
2022-02-15 09:15:30 +0200

    chore: update ket to v2022.02.15

--- ket.rb
+++ ket.rb
@@ -2,14 +2,14 @@ class Ket < Formula
 
   desc "KET - Kafka Elasticsearch Tool"
   homepage "https://github.com/vinted/kafka-elasticsearch-tool"
-  version "2021.09.24"
+  version "2022.02.15"
 
   if OS.linux?
-    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.09.24/ket-v2021.09.24-linux-amd64.zip"
-    sha256 "c55409b9860a8d8f8c50e08aa2c17203c62bb7c42d95cebf3c363c5feed61ab9"
+    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2022.02.15/ket-v2022.02.15-linux-amd64.zip"
+    sha256 "d70715a42fb7882542c288ca67eea0712aa3d2e65523595fc9142a1c480496da"
   else
-    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2021.09.24/ket-v2021.09.24-macos-amd64.zip"
-    sha256 "2c82b06039f6f0aa5899ff0c9809fad7cac186087980cc0904da766b470d8b7e"
+    url "https://github.com/vinted/kafka-elasticsearch-tool/releases/download/v2022.02.15/ket-v2022.02.15-macos-amd64.zip"
+    sha256 "e9d6638fd637e0594013c048de1e7100b4a25e2437b467b0c3e5a03090eb1e71"
   end
 
   def install
