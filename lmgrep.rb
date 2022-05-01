be49d831d8b6a2a14e0affa2c9665f282173a6d4
Dainius Jocas <dainius.jocas@gmail.com>
2022-05-01 23:21:47 +0300

    fix: macos aarch64 sha

--- lmgrep.rb
+++ lmgrep.rb
@@ -10,10 +10,10 @@ class Lmgrep < Formula
   else
     if Hardware::CPU.arm?
       url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.05.01/lmgrep-v2022.05.01-macos-aarch64.zip"
-      sha256 "37f09bb69b55c6422292692732ad81b6762c0bbafc52d3bee450da0758bd7932"
+      sha256 "be0d038c5218f77dc6e0b258d9da51ae88041ae08ff906abc1005095f808fc4a"
     else
       url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.05.01/lmgrep-v2022.05.01-macos-amd64.zip"
-      sha256 "${MACOS_AARCH64_SHA}"
+      sha256 "37f09bb69b55c6422292692732ad81b6762c0bbafc52d3bee450da0758bd7932"
     end
   end
 
