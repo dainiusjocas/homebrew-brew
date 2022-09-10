bda875e411a7f311b6175e237cfd81e03623ca83
Dainius Jocas <dainius.jocas@gmail.com>
2022-09-10 20:31:16 +0300

    bump lmgrep

--- lmgrep.rb
+++ lmgrep.rb
@@ -6,14 +6,14 @@ class Lmgrep < Formula
 
   if OS.linux?
     url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.08.22/lmgrep-v2022.08.22-linux-amd64-static.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "672848ac0601e745f36417720a75bbf681c2cafc1d28a512323ffcdac686433f"
   else
     if Hardware::CPU.arm?
       url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.08.22/lmgrep-v2022.08.22-macos-aarch64.zip"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "a438132b80d5012158f437071e7d2bc66b0df671718655e4363f12917b996b4e"
     else
       url "https://github.com/dainiusjocas/lucene-grep/releases/download/v2022.08.22/lmgrep-v2022.08.22-macos-amd64.zip"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "309f9fe4395f4df94278037bcd64663232bce4ddf18f61af04dbfa01be2f497a"
     end
   end
 
