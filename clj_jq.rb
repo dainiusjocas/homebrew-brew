2f5010e120fac35a8203a721b20a436f87c1568a
Dainius Jocas <dainius.jocas@gmail.com>
2022-10-24 12:08:36 +0300

    update clj-jq to 1.1.5

--- clj_jq.rb
+++ clj_jq.rb
@@ -2,14 +2,14 @@ class CljJq < Formula
 
   desc "`jackson-jq` based command-line JSON processor"
   homepage "https://github.com/dainiusjocas/clj-jq"
-  version "1.1.4"
+  version "1.1.5"
 
   if OS.linux?
-    url "https://github.com/dainiusjocas/clj-jq/releases/download/v1.1.4/clj-jq-1.1.4-linux-amd64-static.zip"
-    sha256 "767e33fec343689a035029c60de3916d313c2593d7f23775c6c77f76fd861830"
+    url "https://github.com/dainiusjocas/clj-jq/releases/download/v1.1.5/clj-jq-1.1.5-linux-amd64-static.zip"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   else
-    url "https://github.com/dainiusjocas/clj-jq/releases/download/v1.1.4/clj-jq-1.1.4-macos-amd64.zip"
-    sha256 "c8d187cd511432d08895b1e58d946bdeb5cf6f77ec2fceed24918da02090d5f7"
+    url "https://github.com/dainiusjocas/clj-jq/releases/download/v1.1.5/clj-jq-1.1.5-macos-amd64.zip"
+    sha256 "e9c66b02647933bdf5fcc51250ef913a7eaaa99cd364885e081bb675e14b31f6"
   end
 
   def install
