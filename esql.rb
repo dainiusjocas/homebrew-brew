debe3ab4f7338c7c1792431fce1f4bd457b4548d
Dainius Jocas <dainius.jocas@gmail.com>
2023-06-19 00:49:51 +0300

    update esql to 1.0.13

--- esql.rb
+++ esql.rb
@@ -2,18 +2,18 @@ class Esql < Formula
 
   desc "ESQL: CLI tool to query the Elasticsearch SQL API"
   homepage "https://github.com/dainiusjocas/elasticsearch-sql"
-  version "1.0.10"
+  version "1.0.13"
 
   if OS.linux?
-    url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.10/esql-v1.0.10-linux-amd64-static.zip"
-    sha256 "d504fe6ce38ab017d3fe586f10c48063ddd192fcc0f7204d64028fb7b5ea1f39"
+    url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.13/esql-v1.0.13-linux-amd64-static.zip"
+    sha256 "9984a798639dd71fa7a94449fd82e096d7f675916da5812a006d308c3e6aa7ac"
   else
     if Hardware::CPU.arm?
-      url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.10/esql-v1.0.10-macos-aarch64.zip"
-      sha256 "eaf8907c0d28dd7737182d3bf0905e830cab6844d232bd0aeb38174cab7852a8"
+      url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.13/esql-v1.0.13-macos-aarch64.zip"
+      sha256 "b6dcdf101b08803c4ea9976e44d828b3ebfb851206ee3d8dae01007969ebce88"
     else
-      url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.10/esql-v1.0.10-macos-amd64.zip"
-      sha256 "b4027919f0a89bf7ac07b99dc6d6be44af844bf5e0537f72c414a393ac01b962"
+      url "https://github.com/dainiusjocas/elasticsearch-sql/releases/download/v1.0.13/esql-v1.0.13-macos-amd64.zip"
+      sha256 "23a3b5d8b716cadffac0970160a3ce6c2b80be3c954dd279e2e38c4b36cbdebc"
     end
   end
 
